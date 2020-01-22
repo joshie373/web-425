2518b6cde1396174f46518b77cc64c133b231dbc
Joshua Hughes <joshie373@gmail.com>
2020-01-21 22:36:16 -0700

    updating 4.3

--- week-4/hughes-form-control/src/app/app.module.ts
+++ week-4/hughes-form-control/src/app/app.module.ts
@@ -1,8 +1,11 @@
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
